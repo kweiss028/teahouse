678ecfa9a5920a50617a8b32907f283a23cedb14
oz123 <nahumoz@gmail.com>
2014-11-02 05:40:33 +0100

    Modify the code to be a bit more pythonic

--- mymodule.py
+++ mymodule.py
@@ -1,32 +1,31 @@
 import time
 
+
 class Cup(object):
     def __init__(self, drink, price):
         self.content = drink
         self.price = price
-        self.temp = 80
+        self._init_temp = 80
         self.full = 1.0
-        self.timestamp = time.time()    #initiate time for cool-down
-    
+        self._timestamp = time.time()  # initiate time for cool-down
+
     def heat_up(self, degrees):
         if degrees < 0:
             raise Exception('heat_up accepts only positive numbers')
-        new_temp = self.get_temp() + degrees
+        new_temp = self.temp + degrees
         if new_temp <= 100:
-            self.temp = new_temp
+            self._init_temp = new_temp
         else:
-            self.temp = 100
-        self.timestamp = time.time()    #refresh time for cool-down
-            
+            self._init_temp = 100
+        self._timestamp = time.time()   # refresh time for cool-down
+
     def drink(self, sip):
         if sip < 0:
             raise Exception('drink only accepts positive numbers')
         self.full -= sip
         if self.full <= 0:
             self.full = 0
-            
-    def get_temp(self):
-        cooled = int(time.time() - self.timestamp)
-        self.temp -= cooled
-        self.timestamp = time.time()    #refresh time for cool-down
-        return self.temp
+
+    @property
+    def temp(self):
+        return self._init_temp - round(1*(time.time() - self._timestamp))
--- test_mymodule.py
+++ test_mymodule.py
@@ -3,43 +3,46 @@ import time
 from mymodule import Cup
 
 teacup = Cup('tea', 1.80)
-teaTime = int(teacup.timestamp)
+teaTime = int(teacup._timestamp)
 
 coffee = Cup('coffee', 2.10)
-coffeeTime = int(coffee.timestamp)
+coffeeTime = int(coffee._timestamp)
+
 
 class TestMyModule(unittest.TestCase):
 
     def test_has_attribute(self):
         self.assertTrue(hasattr(teacup, 'full'))
         self.assertFalse(hasattr(teacup, 'fill'))
-    
+
     def test_drink(self):
         teacup.drink(0.2)
         self.assertRaises(Exception, teacup.drink, -2)
-        self.assertEqual(0.8, teacup.full) 
-        
+        self.assertEqual(0.8, teacup.full)
+
     def test_heat_up(self):
-        self.assertRaises(Exception, teacup.heat_up, -20)  
-        self.assertEqual(teacup.temp, 80)
-        teacup.timestamp = time.time()  # cup should not cool down before this test
+        self.assertRaises(Exception, teacup.heat_up, -20)
+        self.assertTrue(teacup.temp < 80)
+        # cup should not cool down before this test
+        #teacup.timestamp = time.time()
+        temp = teacup.temp
         teacup.heat_up(10)
-        self.assertEqual(teacup.temp, 90)     
-    
+        self.assertEqual(teacup.temp, temp + 10)
+
     def test_has_timestamp(self):
-        self.assertTrue(hasattr(teacup,'timestamp'))
-    
+        self.assertTrue(hasattr(teacup, '_timestamp'))
+
     def test_cool_down(self):       # some tolerance for timer problems
         self.assertEqual(coffee.temp, 80)
         time.sleep(2)
-        startTemp = coffee.get_temp()
+        startTemp = coffee.temp
         self.assertTrue(startTemp < 80)
         coffee.heat_up(20)
         time.sleep(2)
-        endTemp = coffee.get_temp()        
+        endTemp = coffee.temp
         self.assertTrue(endTemp > startTemp)
         self.assertTrue(endTemp < startTemp + 20)
-        
-          
+
+
 if __name__ == '__main__':
     unittest.main()
