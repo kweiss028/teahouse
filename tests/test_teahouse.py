2b4c088fce808bb6f6c1a6f3bbfe271278bb9973
kweiss028 <kweiss028@gmail.com>
2014-11-19 14:51:34 +0100

    improved tests

--- tests/test_teahouse.py
+++ tests/test_teahouse.py
@@ -21,44 +21,44 @@ def almost_equal(result_temp, expected_temp):    # tolerance for function slowne
 class TestTeahouse(unittest.TestCase):
 
     def setUp(self):
-        teacup = CoffeeMachine.brew('tea')
-        coffee_cup = CoffeeMachine.brew('coffee')
-        bread_cup = CoffeeMachine.brew('bread')
+        self.teacup = CoffeeMachine.brew('tea')
+        self.coffee_cup = CoffeeMachine.brew('coffee')
+        self.bread_cup = CoffeeMachine.brew('bread')
 
     def test_has_attribute(self):
-        self.assertTrue(hasattr(teacup, 'full'))
-        self.assertFalse(hasattr(teacup, 'fill'))
+        self.assertTrue(hasattr(self.teacup, 'full'))
+        self.assertFalse(hasattr(self.teacup, 'fill'))
 
     def test_wrong_drink(self):
-        self.assertIsInstance(bread_cup, str)
-        self.assertFalse(hasattr(bread_cup, 'price'))
+        self.assertIsInstance(self.bread_cup, str)
+        self.assertFalse(hasattr(self.bread_cup, 'price'))
 
     def test_right_drink(self):
-        self.assertIsInstance(teacup, object)
-        self.assertTrue(hasattr(teacup, 'price'))
-        self.assertEqual(teacup.price, 1.80)
+        self.assertIsInstance(self.teacup, object)
+        self.assertTrue(hasattr(self.teacup, 'price'))
+        self.assertEqual(self.teacup.price, 1.80)
         
     def test_drink(self):
-        teacup.drink(0.2)
-        self.assertRaises(Exception, teacup.drink, -2)
-        self.assertEqual(0.8, teacup.full)
+        self.teacup.drink(0.2)
+        self.assertRaises(Exception, self.teacup.drink, -2)
+        self.assertEqual(0.8, self.teacup.full)
 
     def test_heat_up(self):
-        self.assertRaises(Exception, teacup.heat_up, -20)
-        self.assertTrue(teacup.temp < 80)
-        temp = teacup.temp
-        teacup.heat_up(10)
-        self.assertTrue(almost_equal(teacup.temp, temp + 10))    
+        self.assertRaises(Exception, self.teacup.heat_up, -20)
+        self.assertTrue(self.teacup.temp < 80)
+        temp = self.teacup.temp
+        self.teacup.heat_up(10)
+        self.assertTrue(almost_equal(self.teacup.temp, temp + 10))    
 
     def test_has_timestamp(self):
-        self.assertTrue(hasattr(teacup, '_timestamp'))
+        self.assertTrue(hasattr(self.teacup, '_timestamp'))
 
     def test_cool_down(self):    
-        self.assertEqual(coffee_cup._init_temp, 80)
-        coffee_cup._timestamp -= 1200    # set back start time by 20 minutes
-        start_temp = coffee_cup.temp
-        coffee_cup.heat_up(20)
-        self.assertTrue(almost_equal(coffee_cup.temp, start_temp + 20))
+        self.assertEqual(self.coffee_cup._init_temp, 80)
+        self.coffee_cup._timestamp -= 1200    # set back start time by 20 minutes
+        start_temp = self.coffee_cup.temp
+        self.coffee_cup.heat_up(20)
+        self.assertTrue(almost_equal(self.coffee_cup.temp, start_temp + 20))
 
     def tearDown(self):
         pass
